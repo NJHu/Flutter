3995a0a861ff42d85f3cef0750cc07b215d2a2f5
胡鹏 <64hp@163.com>
2020-09-18 15:07:23 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
-//主函数（入口函数），下面我会简单说说Dart的函数
-void main() => runApp(MyApp());
-
 // 声明MyApp类
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
@@ -24,6 +21,10 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
+//主函数（入口函数），下面我会简单说说Dart的函数
+void main() => runApp(MyApp());
 
 class RandomWords extends StatefulWidget {
   @override
